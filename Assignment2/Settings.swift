be0105152f00601a955333db417ba424054e49c2
SnIvan <ivan.sakhan@icloud.com>
2019-04-04 17:01:49 -0400

    added physics and scorelabel

---
+++ Assignment2/Settings.swift
@@ -0,0 +1,16 @@
+//
+//  Settings.swift
+//  Assignment2
+//
+//  Created by Ivan on 2019-04-04.
+//  Copyright © 2019 CentennialCollege. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+enum PhysicsCategories {
+    static let none: UInt32 = 0
+    static let ballCategory: UInt32 = 0x1
+    static let switchCategory: UInt32 = 0x1 << 1
+}
